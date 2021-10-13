a1f8a0bc0763601e8a1709c0e68753b3369be5c3
Asochka <fadeevafadeeva03@gmail.com>
2021-10-13 23:08:00 +0300

    +

--- functions/ft_strrchr.c
+++ functions/ft_strrchr.c
@@ -6,15 +6,25 @@ char *ft_strrchr(const char *str, int ch)
 	int i;
 	char *res;
 
-	i = ft_strlen(str) - 1;
-	while (i >= 0)
+	res = (char *)str;
+	while (*res)
+		res++;
+	while (*res != ch)
 	{
-		if (str[i] == ch)
-			return (str + i)
-		i--;
+		if (res < (char *)str)
+			return NULL;
+		res--;
 	}
-	if (ch == 0)
-		return (str + ft_strlen(str));
-	else
-		return NULL;
+	return (res);
+}
+
+int main()
+{
+	char lotr[] = "_-=Властлин клец=-_";
+	char *ring0 = ft_strrchr(lotr, '0');
+	char *ring1 = strrchr(lotr, '0');                   
+
+	printf("%lu ", ring0 - lotr + 1);
+	printf("%lu ", ring1 - lotr + 1);
+	return 0;
 }
