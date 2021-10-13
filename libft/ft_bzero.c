e5851157af1438130f8dbee8ddb3fe79486cac8e
Asochka <fadeevafadeeva03@gmail.com>
2021-10-13 21:42:56 +0300

    com

--- libft/ft_bzero.c
+++ libft/ft_bzero.c
@@ -21,8 +21,8 @@ int main()
 {
 	char str1[] = "123456789";
 	char str2[] = "123456789";
-	bzero(str1, 1);
+	//bzero(str1, 1);
 	ft_bzero(str2, 1);
-	printf("%s\n", str1);
+	//printf("%s\n", str1);
 	printf("%s", str2);
 }
