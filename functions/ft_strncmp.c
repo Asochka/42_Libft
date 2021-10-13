7e11a7ff8e46e77ccba69125530f730a30a58d51
Asochka <fadeevafadeeva03@gmail.com>
2021-10-14 01:40:53 +0300

    1

--- functions/ft_strncmp.c
+++ functions/ft_strncmp.c
@@ -1,9 +1,33 @@
 #include "header.h"
 
-int strncmp(const char *string1, const char *string2, size_t num)
+int ft_strncmp(const char *string1, const char *string2, size_t num)
 {
 	size_t i;
 
 	i = 0;
-	while (i < num) && (*string1)
+	while ((i < num) && (string1[i] == string2[i]))
+		i++;
+	if (i == (num - 1))
+		return (0);
+	else if (string1[i] > string2[i])
+		return (1);
+	else
+		return (-1);
+}
+
+int main()
+{
+	char str1[16]="1234567890";
+	char str2[16]="1234507890";
+  
+	if (strncmp (str1, str2,4)==0)
+		printf("strings are equal\n");
+	else
+		printf("strings aren't equal\n");
+
+	if (ft_strncmp (str1, str2,4)==0)
+		printf("strings are equal\n");
+	else
+		printf("strings aren't equal\n");
+	return 0;
 }
